82f7f5bdecd804d5ee9e70bd1c065e41d9e89da2
Daniil Lyakhov <71712454+nxturnvl@users.noreply.github.com>
2023-12-21 15:10:13 +0300

    docs : added documentation into files and updated README.md

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''принимает радиус круга r, возвращает его площадь'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''принимает радиус круга r, возвращает его периметр'''
     return 2 * math.pi * r
 
